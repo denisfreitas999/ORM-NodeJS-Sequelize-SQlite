c29e9fabe5c49a310281fd36cfa7e1c6ade8babb
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-13 17:25:08 -0300

    Adicionando Mixins para criar associações e poder consultar as matrículas de um determinado estudante

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -7,6 +7,18 @@ class PessoaController extends Controller {
   constructor() {
     super(pessoasServices);
   }
+
+  async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas = await pessoasServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json({ message: 'Matriculas buscadas com sucesso.', listaMatriculas });
+    } catch (error) {
+      // Tratamento de erro
+      return res.status(500).json({ message: 'Erro no servidor.' });
+    }
+  }
+
 }
 
 module.exports = PessoaController;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const pessoas = require('./pessoaRoutes');
+const pessoas = require('./pessoaRoutes.js');
+const categorias = require('./categoriaRoutes.js');
+const cursos = require('./cursoRoutes.js');
 
 module.exports = (app) => {
   app.use(
     express.json(),
-    pessoas
+    pessoas,
+    categorias,
+    cursos,
   );
 };
