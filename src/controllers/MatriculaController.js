933b0ead14dc7edce57dbe68aaadaa669e19f58b
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-14 14:58:02 -0300

    Realizando contagem de matriculas por pessoa | Funções agregadoras

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -7,6 +7,19 @@ class MatriculaController extends Controller {
   constructor() {
     super(matriculasServices);
   }
+
+  async pegaMatriculasPorEstudante(req, res) {
+    const { estudante_id } = req.params;
+    try {
+      const listaMatriculasPorEstudante = await matriculasServices.pegaEContaRegistros({
+        estudante_id: Number(estudante_id),
+        status: 'matriculado'
+      });
+      return res.status(200).json({ message: 'Busca realizada com sucesso.', listaMatriculasPorEstudante });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 }
 
 module.exports = MatriculaController;
